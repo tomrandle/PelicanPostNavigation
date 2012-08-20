771fae6a6113b85bf893549bab394a379d5cf2e4
Tom Randle <tom@Toms-MacBook-Pro.local>
2012-08-20 19:02:41 +0100

    Returns html

--- pelicanButtons.py
+++ pelicanButtons.py
@@ -10,6 +10,27 @@ for fileName in glob.glob("*.html"):
 
 print fileList
 
+
+for fileName in fileList:
+	fileTxt = open(fileName)
+
+	print fileTxt.read()
+
+
+"""
+  <header>
+    <h2 class="entry-title">
+      <a href="../posts/my-new-post--.html" rel="bookmark"
+         title="Permalink to Mr Fox is an arse.">Mr Fox is an arse.</a></h2>
+  </header>
+  <footer class="post-info">
+    <abbr class="published" title="2012-08-20T18:23:03">
+      Mon 20 August 2012
+    </abbr>
+"""
+
+
+
 """
 txt = open(filename)
 
